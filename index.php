cf7299eea94c7456ebbb5e6e28d4ed42db995c58
Chino <beto4600@gmail.com>
2018-04-14 17:52:26 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DBProject</title>
+	<!-- Materialize -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="app.css">        
+</head>
+<body>
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+	require 'index.php';
+?>
+
+<div class="row">
+	<br>
+	<br>
+	<br>
+	<br>
+	<div class="card col s6 offset-s3 padded center">
+		<div class="input-field s12">
+			<input id="id" type="text">
+			<label for="id">Nómina</label>
+		</div>
+		<div class="input-field s12">
+			<input id="pass" type="text">
+			<label for="pass">Contraseña</label>
+		</div>
+		<button class="btn waves-effect btn-large">Iniciar Sesión</button>
+	</div>
+</div>
